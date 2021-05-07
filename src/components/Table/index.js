fb879ad6f7d06e04a1a5fdd8eabaded18111bd6b
rjkenney11 <rachelkenney@Rachels-MacBook-Pro.local>
2021-05-07 17:02:57 -0400

    search bar had me crying

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -15,21 +15,21 @@ class DataTable extends Component {
         // Make A Request for that Data (from the API)
         API.getEmployees().then((res) => {
             console.log(res);
-                const mapEmployees = res.data.results.map((emp) =>{
-                    return {
-                        id: emp.id.value,
-                        pic: emp.picture.thumbnail,
-                        firstName: emp.name.first,
-                        lastName: emp.name.last,
-                        age: emp.dob.age,
-                        phone: emp.phone,
-                        email: emp.email
-                    }
-                });
-                // Once we have the data --> Update our STATE 
-                this.setState({ employees: mapEmployees });
-                this.setState({ ...this.state, filteredEmployees: this.state.employees})
-            })
+            const mapEmployees = res.data.results.map((emp) => {
+                return {
+                    id: emp.id.value,
+                    pic: emp.picture.thumbnail,
+                    firstName: emp.name.first,
+                    lastName: emp.name.last,
+                    age: emp.dob.age,
+                    phone: emp.phone,
+                    email: emp.email
+                }
+            });
+            // Once we have the data --> Update our STATE 
+            this.setState({ employees: mapEmployees });
+            this.setState({ ...this.state, filteredEmployees: this.state.employees })
+        })
             .catch(err => {
                 console.log(err);
             })
@@ -37,41 +37,103 @@ class DataTable extends Component {
     }
 
 
-    // Display Component View
-    render() {
-        console.log(this.state);
+    filterList = e => {
+        const filterTarget = e.target.value;
+        let employeeList = this.state.employees;
+        employeeList = employeeList.filter(i => {
+            let values = Object.values(i)
+                .join("")
+                .toLocaleLowerCase();
+            return values.indexOf(filterTarget.toLowerCase()) !== -1;
+        })
+        this.setState({ filteredEmployees: employeeList });
+    }
 
+    render() {
         return (
-            <body>
-                <h1></h1>
-                <div className="employee-list">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th className="pic">Profile Picture</th>
-                                <th className="fname">First Name</th>
-                                <th className="lname">Last Name</th>
-                                <th className="age">Age</th>
-                                <th className="phone">Phone #</th>
-                                <th className="email">Email</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {this.state.filteredEmployees.map(emp =>(<tr>
-                             <td><img src={emp.pic} className="thumbnail" alt="thumbnail" /></td>
-                             <td>{emp.firstName}</td>
-                             <td>{emp.lastName}</td>
-                             <td>{emp.age}</td>
-                             <td>{emp.phone}</td>
-                             <td>{emp.email}</td>
-                             </tr>))}
-                        </tbody>
-                    </table>
-                </div>
-            </body>
+            <div className="filter-list">
+                <input className="search" type="text" placeholder='Search directory for Employee' onChange={this.filterList} />
+                <body>
+                    <h1></h1>
+                    <div className="employee-list">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th className="pic">Profile Picture</th>
+                                    <th className="fname">First Name</th>
+                                    <th className="lname">Last Name</th>
+                                    <th className="age">Age</th>
+                                    <th className="phone">Phone #</th>
+                                    <th className="email">Email</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.filteredEmployees[0] ?
+
+                                        this.state.filteredEmployees.map(emp => (
+                                            <tr>
+                                                <td><img src={emp.pic} className="thumbnail" alt="thumbnail" /></td>
+                                                <td>{emp.firstName}</td>
+                                                <td>{emp.lastName}</td>
+                                                <td>{emp.age}</td>
+                                                <td>{emp.phone}</td>
+                                                <td>{emp.email}</td>
+                                            </tr>
+                                        ))
+                                        :
+                                        this.state.employees.map(emp => (
+                                            <tr>
+                                                <td><img src={emp.pic} className="thumbnail" alt="thumbnail" /></td>
+                                                <td>{emp.firstName}</td>
+                                                <td>{emp.lastName}</td>
+                                                <td>{emp.age}</td>
+                                                <td>{emp.phone}</td>
+                                                <td>{emp.email}</td>
+                                            </tr>
+                                        ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </body>
+
+            </div>
         )
     }
 }
+
+
+
+
+//     // Display Component View
+//     render() {
+//         console.log(this.state);
+
+//         return (
+//             <body>
+//                 <h1></h1>
+//                 <div className="employee-list">
+//                     <table>
+//                         <thead>
+//                             <tr>
+//                                 <th className="pic">Profile Picture</th>
+//                                 <th className="fname">First Name</th>
+//                                 <th className="lname">Last Name</th>
+//                                 <th className="age">Age</th>
+//                                 <th className="phone">Phone #</th>
+//                                 <th className="email">Email</th>
+//                             </tr>
+//                         </thead>
+//                         <tbody>
+//   
+//                         </tbody>
+//                     </table>
+//                 </div>
+//             </body>
+//         )
+//     }
+// }
 
 
 export default DataTable;
